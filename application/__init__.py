053eddf288d4a66a9e1496a37b9a94c52371ad3a
Davi Bernardo <sbernardo.davi@outlook.com>
2020-12-22 20:33:43 -0300

    feat: Criação de arquivos

--- application/__init__.py
+++ application/__init__.py
@@ -5,3 +5,5 @@ template_folder = os.path.abspath('application/view/templates')
 static_folder = os.path.abspath('application/view/static')
 
 app = Flask(__name__,template_folder = template_folder, static_folder = static_folder)
+
+from application.controller import index_controller 
