ab0bdf4877df01c88007a0dc58eddd2428e373a8
Davi Bernardo <sbernardo.davi@outlook.com>
2020-12-23 00:54:30 -0300

    fix: Correção de bugs

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 import os
+from application.model.entity.produto import Produto
+
 
 template_folder = os.path.abspath('application/view/templates')
 static_folder = os.path.abspath('application/view/static')
 
 app = Flask(__name__,template_folder = template_folder, static_folder = static_folder)
 
-from application.controller import index_controller 
+
+from application.controller import index_controller
--- application/controller/index_controller.py
+++ application/controller/index_controller.py
@@ -1,9 +1,12 @@
 from flask import render_template, request
 from application import app 
+from application.model.entity.produto import Produto
+from application.model.dao.produto_dao import ProdutoDAO
 
 
 
-@app.route('/')
-@app.route('/home')
-def home():
-    return render_template('index.html')
+
+@app.route("/")
+def index():
+    listaProdutos = ProdutoDAO().get_listar_Produtos()
+    return render_template("index.html", listaProdutos = listaProdutos)
